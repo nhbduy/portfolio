2d8519bf9a16ff9dee20f5119e7612a15a2ad158
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2020-04-02 18:41:12 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8ddd2417e3f4c974fcec.js"
+    "url": "webpack-runtime-c0fec5665eb5a6be5b8f.js"
   },
   {
     "url": "framework-7e127b281060aec365c4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "660de4dc1140a97610a2c60ca1146fe3"
+    "revision": "66c24ed8cdd64d441585cb645b5dee66"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8f20f2c7b8ac61d8df98.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "7033c365a7f6b866a9b428aad36f43be"
+    "revision": "0fb883092bfbcba86be5aca9daf82118"
   },
   {
     "url": "manifest.webmanifest",
